5d55c8070e53f7356388610cdf25e03362da994c
Toaster <87452263+ToasterWithTheBread@users.noreply.github.com>
2024-06-05 12:54:55 -0600

    realtime.js aktualisieren

--- src/pages/api/realtime.js
+++ src/pages/api/realtime.js
@@ -5,7 +5,7 @@ export default function socketHandler(req, res) {
         const io = new Server(res.socket.server, { maxHttpBufferSize: 1e9 })
         res.socket.server.io = io
 
-        io.on('connection', socket => {
+        io.on("connection", socket => {
             socket.on("room", msg => {
                 socket.join(msg.room_id);
             });
